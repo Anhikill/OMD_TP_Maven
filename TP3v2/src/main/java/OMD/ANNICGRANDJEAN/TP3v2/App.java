5021817d7ed5e8e486847bda6da81315a7e35e38
Ki <Ki@ECHO-DEMONIAQUE>
2022-10-07 11:16:33 +0200

    ajout projet

---
+++ TP3v2/src/main/java/OMD/ANNICGRANDJEAN/TP3v2/App.java
@@ -0,0 +1,13 @@
+package OMD.ANNICGRANDJEAN.TP3v2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TP3v2/src/test/java/OMD/ANNICGRANDJEAN/TP3v2/FirstPdfTest.java
@@ -0,0 +1,19 @@
+package OMD.ANNICGRANDJEAN.TP3v2;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FirstPdfTest {
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+//	
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+}
